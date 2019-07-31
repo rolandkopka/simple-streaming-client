cf3f084577a93b4ef3032980180028df8d184217
Roland Kopka <kopkaroland@gmail.com>
2019-07-31 16:45:08 +0200

    Materialize form

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import { TextField, Button, Container, CssBaseline } from "@material-ui/core";
 
 export class StreamCreate extends Component {
-  renderTextInput({ input }) {
-    return <input {...input} />;
+  renderTextField = ({ input, ...custom }) => {
+    return <TextField {...input} {...custom} />;
+  };
+
+  onSubmit(formValues) {
+    console.log(formValues);
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderTextInput} />
-        <Field name="description" component={this.renderTextInput} />
-      </form>
+      <Container>
+        <CssBaseline />
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <div className="container">
+            <Field
+              name="title"
+              component={this.renderTextField}
+              required
+              label="Title"
+            />
+          </div>
+          <div>
+            <Field
+              name="description"
+              component={this.renderTextField}
+              multiline
+              label="Description"
+            />
+          </div>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Container>
     );
   }
 }
