cc3fee968285ef48e5edf1a2284c7753064031c3
Roland Kopka <kopkaroland@gmail.com>
2019-08-03 18:20:53 +0200

    Programmatic Navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 
 import CssBaseline from "@material-ui/core/CssBaseline";
 
@@ -10,18 +10,20 @@ import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
 
+import history from "../history";
+
 const App = () => {
   return (
     <>
       <CssBaseline />
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Route exact path="/" component={StreamList} />
         <Route exact path="/streams/new" component={StreamCreate} />
         <Route exact path="/streams/edit" component={StreamEdit} />
         <Route exact path="/streams/delete" component={StreamDelete} />
         <Route exact path="/streams/show" component={StreamShow} />
-      </BrowserRouter>
+      </Router>
     </>
   );
 };
