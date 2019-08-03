c210aacb29875a42b1fddce33696d2f2fc53fe0f
Roland Kopka <kopkaroland@gmail.com>
2019-08-03 19:51:04 +0200

    Refactor StreamCreate and add a common StreamForm

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,75 +1,25 @@
 import React, { Component } from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
-import { TextField, Button, Container } from "@material-ui/core";
+import StreamForm from "./StreamForm";
+import { Typography } from "@material-ui/core";
 
 export class StreamCreate extends Component {
-  renderTextField = ({ input, meta, ...custom }) => {
-    return (
-      <>
-        <TextField
-          error={meta.touched && meta.invalid}
-          helperText={meta.touched && meta.error}
-          {...input}
-          {...custom}
-        />
-      </>
-    );
-  };
-
   onSubmit = formValues => {
     this.props.createStream(formValues);
   };
 
   render() {
     return (
-      <Container>
-        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-          <div className="container">
-            <Field
-              name="title"
-              component={this.renderTextField}
-              required
-              label="Title"
-            />
-          </div>
-          <div>
-            <Field
-              name="description"
-              component={this.renderTextField}
-              required
-              multiline
-              label="Description"
-            />
-          </div>
-          <Button type="submit">Submit</Button>
-        </form>
-      </Container>
+      <>
+        <Typography variant="h3">Create a Stream</Typography>
+        <StreamForm onSubmit={this.onSubmit} />
+      </>
     );
   }
 }
 
-const validate = formValues => {
-  const errors = {};
-
-  if (!formValues.title) {
-    errors.title = "You must enter a title";
-  }
-
-  if (!formValues.description) {
-    errors.description = "You must enter a description";
-  }
-
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: "streamCreate",
-  validate
-})(StreamCreate);
-
 export default connect(
   null,
   { createStream }
-)(formWrapped);
+)(StreamCreate);
---
+++ src/components/streams/StreamForm.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { TextField, Button, Container } from "@material-ui/core";
+
+export class StreamForm extends Component {
+  renderTextField = ({ input, meta, ...custom }) => {
+    return (
+      <>
+        <TextField
+          error={meta.touched && meta.invalid}
+          helperText={meta.touched && meta.error}
+          {...input}
+          {...custom}
+        />
+      </>
+    );
+  };
+
+  onSubmit = formValues => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    return (
+      <Container>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <div className="container">
+            <Field
+              name="title"
+              component={this.renderTextField}
+              required
+              label="Title"
+            />
+          </div>
+          <div>
+            <Field
+              name="description"
+              component={this.renderTextField}
+              required
+              multiline
+              label="Description"
+            />
+          </div>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Container>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "streamForm",
+  validate
+})(StreamForm);
